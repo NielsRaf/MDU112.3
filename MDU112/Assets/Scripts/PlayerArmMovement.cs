c097b59f96c1a888d9d50970fb1cf65a75d2ffea
NielsRaf <1007052@student.sae.edu.au>
2016-12-01 15:59:01 +1000

    Added arms, player prefab, enemy prefab and collision.

---
+++ MDU112/Assets/Scripts/PlayerArmMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerArmMovement : MonoBehaviour 
+{
+
+    private Vector2 userInput;
+    public float rotationSpeed = 100f;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        float horizontal = Input.GetAxis("Vertical");
+
+        // store the axes in the user input variable
+        userInput = new Vector2(horizontal, 0);
+
+        // Method 3 - Part a (rotation)
+        Vector3 rotationDelta = new Vector3(0, 0, -horizontal * rotationSpeed * Time.deltaTime);
+        transform.Rotate(rotationDelta);
+
+	}
+}
---
+++ MDU112/Assets/Scripts/PlayerPickUp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPickUp : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D Enemy)
+    {
+        if (Enemy.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit");
+            Enemy.transform.parent = gameObject.transform;
+            
+        }
+    }
+}
